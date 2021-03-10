f76c46e61cdad4e9c0bc25514e542d1d2eecd28e
Jules Bouvet <j.bouvet99@gmail.com>
2021-03-10 17:55:21 +0100

    add generation sinus function

--- src/raw_audio_generator.py
+++ src/raw_audio_generator.py
@@ -1,6 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
+from raw_audio_autoencoder import AutoEncoder_1D
 
 
 def generate_data(model, nb_samples):
@@ -21,6 +22,31 @@ def generate_data(model, nb_samples):
     return output
 
 
+def generate_sinus(model):
+
+    latent_space_values = [10, 20, 30, 40, 50, 60]
+
+    fig, ax = plt.subplots(2, 3)
+    for ii in range(2):
+        for jj in range(3):
+            # generate random input
+            input = torch.normal(mean=latent_space_values[3*ii+jj], std=1, size=(1, 2, 688))  # shape (1, 2, 688)
+
+            # input goes through the decoder
+            output = model.decoder(input)  # shape (1, 2, 11000)
+
+            # remove the 1st dimension -> (2, 11000)
+            output = torch.squeeze(output)
+            output = output.detach().numpy()
+
+            ax[ii, jj].plot(output[0][:200], color="tab:red")
+            ax[ii, jj].set_title(f"Latent space {latent_space_values[3*ii+jj]}")
+            ax[ii, jj].set_xticks([])
+            ax[ii, jj].set_yticks([])
+    plt.show()
+    return output
+
+
 def from_array_to_wav(sound_array, fs, name):
     sound_array_0, sound_array_1 = sound_array[0], sound_array[1]
     wavfile.write('audio_generated/' + name + 'stereo_0.wav', rate=fs, data=sound_array_0)
@@ -30,9 +56,9 @@ def from_array_to_wav(sound_array, fs, name):
 
 if __name__ == '__main__':
     # load trained model and sinus data
-    model = torch.load('models/raw_audio_encoder_1D.pt')
+    model_name = 'raw_audio_encoder_1D_sinus_200_1000Hz_kernel13_epoch25.pt'
+    model = torch.load('../models/'+model_name)
     model.eval()
-    output = generate_data(model)
-    from_array_to_wav(sound_array=output, fs= 11000, name='test1')
+    output = generate_sinus(model)
 
 
